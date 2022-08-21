83f16873c9b83167eec380079efe0bf35e11a7fc
Max Hebditch <maxhebditch@protonmail.com>
2022-08-21 16:16:11 +0000

    make request for site info

--- src/apiHandlers/siteInfoHandler.ts
+++ src/apiHandlers/siteInfoHandler.ts
@@ -1,5 +1,13 @@
-export const requestSiteInformation = (): void => {
-    return
+import { SiteInfo } from '../types/siteInfoTypes'
+import { RequestResponse } from '../types/requestType'
+
+import axios, { AxiosResponse, AxiosError } from 'axios'
+import config from 'config'
+
+export const requestSiteInformation = async (): Promise<AxiosResponse> => {
+    const siteInfoURL = `${config.get('API.URL')}${config.get('API.SITE_INFO')}`
+    const siteInfoRequest = `${siteInfoURL}?api_key=${process.env.API_KEY}`
+    return await axios.get<SiteInfo>(siteInfoRequest)
 }
 
 export const getSiteInformation = (): void => {
