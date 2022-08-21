598c0e12b3111adb2529e0e2ac4d44e766f7b2f1
Max Hebditch <maxhebditch@protonmail.com>
2022-08-21 14:40:03 +0000

    separate concern of each function and update tests to match

--- src/apiHandlers/getOutageHandler.ts
+++ src/apiHandlers/getOutageHandler.ts
@@ -1,24 +1,19 @@
-import { OutageItem, OutageResponse } from "../types/outageTypes"
-import axios from 'axios'
+import { OutageItem, OutageResponse } from '../types/outageTypes'
+import axios, { AxiosResponse } from 'axios'
 import config from 'config'
 
-export const requestOutages = async(): Promise<OutageItem[]> => {
-    const outagesURL = `${config.get('API.URL')}${config.get('API.OUTAGES_ENDPOINT')}`
+export const requestOutages = async (): Promise<AxiosResponse> => {
+    const outagesURL = `${config.get('API.URL')}${config.get(
+        'API.OUTAGES_ENDPOINT'
+    )}`
     const outagesRequest = `${outagesURL}?api_key=${process.env.API_KEY}`
-    const { data, status } = await axios.get<OutageItem[]>(outagesRequest)
-    return data
+    return await axios.get<OutageItem[]>(outagesRequest)
 }
 
-export const getAllOutages = async(): Promise<OutageResponse> => {
-    let outageData : OutageItem[];
-
-    try {
-        outageData = await requestOutages()
-    } catch(err) {
-        outageData = [{}] as OutageItem[]
-    }
+export const getAllOutages = async (): Promise<OutageResponse> => {
+    const { data, status } = await requestOutages()
     return {
-        status: 200,
-        data: outageData
-    }
+        status: status,
+        data: data
+    } as OutageResponse
 }
--- tests/apiHandlers/getOutageHandler.test.ts
+++ tests/apiHandlers/getOutageHandler.test.ts
@@ -5,21 +5,28 @@ import MockAdapter from 'axios-mock-adapter'
 import config from 'config'
 
 process.env.API_KEY = 'fakekey'
-const outagesURL = `${config.get('API.URL')}${config.get('API.OUTAGES_ENDPOINT')}`
+const outagesURL = `${config.get('API.URL')}${config.get(
+    'API.OUTAGES_ENDPOINT'
+)}`
 const outagesRequest = `${outagesURL}?api_key=${process.env.API_KEY}`
-const mock = new MockAdapter(axios);
+const mock = new MockAdapter(axios)
 
 describe('Test happy path', () => {
-    test('Test requesting outage API items', async () => {
-        mock.onGet(outagesRequest).reply(200, mockAllOutagesItems);
-        expect(await outageHandler.requestOutages()).toEqual(mockAllOutagesItems)
+    beforeAll(() => {
+        mock.onGet(outagesRequest).reply(200, mockAllOutagesItems)
     })
 
-    // test('Test return of all outages', async () => {
-    //     const expectedResponse = {
-    //         status: 200,
-    //         data: mockAllOutagesItems
-    //     }
-    //     expect(outageHandler.getAllOutages()).toEqual(expectedResponse)
-    // })
+    test('Test requestOutages return', async () => {
+        const returned = await outageHandler.requestOutages()
+        expect(returned.data).toEqual(mockAllOutagesItems)
+        expect(returned.status).toEqual(200)
+    })
+    test('Test getAllOutages return', async () => {
+        const expectedResponse = {
+            status: 200,
+            data: mockAllOutagesItems
+        }
+        const returned = await outageHandler.getAllOutages()
+        expect(returned).toEqual(expectedResponse)
+    })
 })
