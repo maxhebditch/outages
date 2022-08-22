fa3dbc88041a242eed39ad8b3b010764933597fc
Max Hebditch <maxhebditch@protonmail.com>
2022-08-22 10:24:58 +0000

    use correct site name

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import { appWrapper } from './appHandlers/runApp'
 
-export const main = async (): Promise<void> => {
-    appWrapper('kingfisher')
+export const main = async (siteId: string): Promise<void> => {
+    console.log(`Requesting ${siteId}`)
+    await appWrapper(siteId)
 }
+
+main('norwich-pear-tree')
